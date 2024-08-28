31263bec4aa10172cc8879ba5203e818ab017a21
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-08-28 15:35:45 -0400

    Complete 02_repeatString
    ~ Completed exercise 02_repeatString, including
    all 7 test stages

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // Initialize variables
+    let resultString = '';
 
+    // For a positive num of times, iterate
+    for (let i=1; i <= num; i++) {
+        // Append to string
+        resultString+=string;
+    }
+
+    // For a negative num of times, return error
+    if (num<0) {
+        return "ERROR";
+    } else {
+        return resultString;
+    }
 };
 
 // Do not edit below this line
