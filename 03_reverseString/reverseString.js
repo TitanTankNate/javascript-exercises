abff9fadf4d6dd8a23de47e20e51edff61399023
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-08-28 15:57:19 -0400

    Complete 03_reverseString
    ~ Completed 03_reverseString as required,
    including all four tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let currentCharIn;
+    let currentCharOut;
+    let newString = [];
+    let oldString = inputString.split('');
 
+    for (let i = 0; i < inputString.length; i++) {
+        // Make new char at index i equal to old char at index -1-i
+        // Choosing old char index -i doesn't work since 0 can't be negative,
+        // but 0 is required to assign the first char in the new array.
+        newString[i] = oldString.at(-1-i);
+    }
+
+    // Reform string from array of chars
+    return newString.join("");
 };
 
 // Do not edit below this line
