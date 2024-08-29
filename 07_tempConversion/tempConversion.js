6b1bd7830ee9deef23d319bd236b7f438834077c
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-08-29 10:19:31 -0400

    Complete 07_tempConversion
    ~ Completed 07_tempConversion as required,
    including all 6 tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let convertedTemp;
+
+  convertedTemp = (5/9) * (tempInF - 32);
+  return Math.round(convertedTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let convertedTemp;
+
+  convertedTemp = ((9/5) * tempInC) + 32;
+  return Math.round(convertedTemp * 10) / 10;
 };
 
 // Do not edit below this line
@@ -9,3 +17,7 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// DEBUG
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(37))
