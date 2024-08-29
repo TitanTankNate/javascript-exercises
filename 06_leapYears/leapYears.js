fc014981169a65ac9c0b192742a6a8b02fd68538
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-08-29 10:06:16 -0400

    Complete 06_leapYears
    ~ Completed 06_leapYears as required, including
    all 6 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    // If year is divisible by 4, year is a leap year
+    if (yearToCheck % 4 == 0){
+        // However, only centuries divisible by 400 are leap centuries
+        if (yearToCheck % 400 == 0){
+            return true;
+        } else if (yearToCheck % 100 == 0) { // Standard centuries are not
+            return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// DEBUG
+//console.log(leapYears(1985));
