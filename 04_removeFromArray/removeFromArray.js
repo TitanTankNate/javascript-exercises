df8c1dd5d9a12674587cea18377f024bf9329887
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-08-29 09:04:14 -0400

    Complete 04_removeFromArray
    ~ Completed 04_removeFromArray as required.
    N I'm making a note here, huge success (lmao), but
    this exercise was a bit challenging, forced me
    to do a bit of googling. Satisfying to complete.
    :)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...elementsToRemove) {
+    // Using the rest function ... allows us to pass an indefinite 
+    // number of arguments containing values to remove
+
+    // For each element in that indefinite array
+    for (const elementToRemove of elementsToRemove) {
+        // Compare each element to the elementToRemove
+        for (let i = 0; i < inputArray.length; i++) {
+            // If the element matches, remove it
+            if (inputArray[i] === elementToRemove) {
+                inputArray.splice(i,1);
+                // Because splice will reduce the array size by one,
+                // we need to re-run the check on the new value now
+                // occupying this index
+                i--;
+            }
+        }
+    }
+
+
+    return inputArray;
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// DEBUGGING
+console.log(removeFromArray([1, 2, 2, 3], 2));
