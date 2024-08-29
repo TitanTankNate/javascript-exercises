434e8a6a35da4b8e73cf5eae33dd436c30476295
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-08-29 09:26:13 -0400

    Complete 05_sumAll
    ~ Completed 05_sumAll exercise and all required
    tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    // Return "ERROR" for negative numbers
+    if (startInt < 0 || endInt < 0) {
+        return "ERROR";
+    }
 
+    // Return "ERROR" for incorrect data types
+    if (typeof(startInt)!== 'number' || typeof(endInt)!== 'number') {
+        return "ERROR";
+    }
+
+    // Return "ERROR" for non-integer numbers
+    if (!Number.isInteger(startInt) || !Number.isInteger(endInt)) {
+        return "ERROR";
+    }
+
+    let result = 0;
+    // Select looping direction based off which argument is larger
+    if (startInt < endInt) {
+        // Loop through all integers from startInt to endInt
+        for (i = startInt;i <= endInt;i++) {
+            result += i;
+            //console.log("Intermediate result: " + result);
+        }
+    } else {
+        // Loop through all integers from endInt to startInt
+        for (i = endInt;i <= startInt;i++) {
+            result += i;
+            //console.log("Intermediate result: " + result);
+        }
+    }
+
+
+    return result;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log("Final result: " + sumAll(3,9));
