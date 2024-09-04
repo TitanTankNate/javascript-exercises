c3e6d8e2c539c03e1a9023c8aca5c8f94f9a9f1b
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-09-04 12:47:18 -0400

    Complete 11_getTheTitles
    + Completed exercise 11_getTheTitles as required

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArray) {
+    // Take in array of book titles
+    // Iterate through each element of the array, pulling out titles only
+    // Add those titles to a "title" array
+    // Return the array
+    const titles = booksArray.map((book) => book.title);
+    return titles;
 
 };
 
