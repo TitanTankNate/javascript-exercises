0fc11230d8809aef4dd1074efdeebb2769dab718
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-09-04 12:18:39 -0400

    Complete 10_fibonacci
    + Completed exercise 10_fibonacci as required

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+// Fibonacci sequence
+// Take "n" input element
+// Create fibonacci sequence per the recurrence relation
+// Fn = Fn-1 + Fn-2 for n > 1
 
+
+const fibonacci = function(n) {
+    // Handle bad input
+    if ((!Number.isInteger(parseInt(n))) || (parseInt(n) < 0)) {
+        return "OOPS";
+    }
+
+    // Create smallest possible fibo array
+    let fiboArray = [0, 1];
+
+    // Calculate the nth element of the array
+    for (i=2;i<=parseInt(n);i++) {
+        // Add that calculation as an element of the array
+        fiboArray.push(fiboArray[i-1] + fiboArray[i-2]);
+    };
+    return fiboArray[parseInt(n)];
 };
+
+// DEBUG TESTING
+console.log(fibonacci(3));
 
 // Do not edit below this line
 module.exports = fibonacci;
