ac7f21cd01a3658b6d457d16ac583590a959d079
Nathan Janota <92643917+TitanTankNate@users.noreply.github.com>
2024-09-04 13:36:55 -0400

    Complete 12_findTheOldest
    + Completed exercise 12_findTheOldest as required

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    // Accept an array of people
+    // Iterate through the array, pulling out their birth and death years
+    const sortedArray = peopleArray.sort(function(personA, personB) {
+        // Retrieve year from today's date
+        const today = new Date().getFullYear();
+
+        // Calculate age based off of years
+        // // Assume current year if death year is not defined
+        const compareFirst = (personA.yearOfDeath == undefined ? today: personA.yearOfDeath) - personA.yearOfBirth;
+        const compareNext =  (personB.yearOfDeath == undefined ? today: personB.yearOfDeath)- personB.yearOfBirth;
+        
+        // Sort the array based off their age
+        return compareFirst > compareNext ? -1 : 1;
+    });
+    // Return the sorted array
+    return sortedArray[0];
 
 };
 
